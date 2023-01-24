670d66dd8b50215109f924660c035a76e6c386ae
Miguel Ángel Durán <miduga@gmail.com>
2023-01-24 21:02:14 +0100

    Add new buttons

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,8 @@
+const { withAnimations } = require('animated-tailwindcss') // eslint-disable-line
 const defaultTheme = require('tailwindcss/defaultTheme') // eslint-disable-line
 
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+module.exports = withAnimations({
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {
@@ -14,4 +15,4 @@ module.exports = {
 		}
 	},
 	plugins: []
-}
+})
