463e58b5fb20c9365f65fb547e99410947d53498
Miguel Ángel Durán <miduga@gmail.com>
2023-01-27 18:59:16 +0100

    Add configurations for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind(), preact()]
 });
