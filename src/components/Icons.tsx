d5ca159013ca11497f20d6569775135828533d7e
Andres Bedoya <andresbedoyag@Andress-MacBook-Pro.local>
2023-01-28 15:19:29 -0500

    remove unnecessary import

--- src/components/Icons.tsx
+++ src/components/Icons.tsx
@@ -1,5 +1,3 @@
-import type { JSX } from "preact/jsx-runtime";
-
 export const Icons = {
   eye: () => (
 		<svg xmlns='http://www.w3.org/2000/svg' width='48' height='48' fill='none' viewBox='0 0 48 48'>
