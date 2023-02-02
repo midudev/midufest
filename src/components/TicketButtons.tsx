74627ab0759102a950c32692ba4c6ad3e6094d94
Ameth Ordoñez <amethgabriel@hotmail.com>
2023-02-02 16:17:46 -0500

    Add twitter button design

--- src/components/TicketButtons.tsx
+++ src/components/TicketButtons.tsx
@@ -1,59 +1,88 @@
-import { useUser } from '@hooks/useUser'
-import { atcb_action } from 'add-to-calendar-button'
+import { useUser } from "@hooks/useUser";
+import { atcb_action } from "add-to-calendar-button";
+import { Icons } from "@components/Icons";
 
-const Button = ({ children, onClick }: { children: string, onClick: any}) => {
+const Button = ({ children, onClick }: { children: string; onClick: any }) => {
 	return (
-		<button class='text-primary text-sm transition bg-zinc-900/90 font-semibold border hover:border-primary border-white/10 rounded-lg px-6 py-3' onClick={onClick}>
+		<button
+			class="text-primary text-sm transition bg-zinc-900/90 font-semibold border hover:border-primary border-white/10 rounded-lg px-6 py-3"
+			onClick={onClick}
+		>
 			{children}
 		</button>
-	)
-}
+	);
+};
 
-export function TicketButtons () {
-	const { user } = useUser()
+const TwitterButton = ({
+	children,
+	onClick,
+}: {
+	children: string;
+	onClick: any;
+}) => {
+	return (
+		<button
+			class="text-white bg-[#1da1f2] hover:bg-[#1da1f2]/90 focus:ring-4 focus:outline-none focus:ring-[#1da1f2]/50 font-medium rounded-lg text-sm px-6 py-3 text-center inline-flex items-center"
+			onClick={onClick}
+		>
+			{children}
+		</button>
+	);
+};
+
+export function TicketButtons() {
+	const { user } = useUser();
 
 	const handleAddCalendar = (e) => {
 		const config = {
-			name: 'miduFest - Conferencia de programación y desarrollo',
-			description: 'Entra a https://twitch.tv/midudev para disfrutar totalmente gratis de la conferencia.',
-			startDate: '2023-03-21',
-			startTime: '17:00',
-			endDate: '2023-03-21',
-			endTime: '22:00',
-			timeZone: 'Europe/Madrid',
-			location: 'https://midufest.com',
-			buttonStyle: 'round',
-			language: 'es',
-			options: ['Google', 'Apple', 'iCal']
-		}
+			name: "miduFest - Conferencia de programación y desarrollo",
+			description:
+				"Entra a https://twitch.tv/midudev para disfrutar totalmente gratis de la conferencia.",
+			startDate: "2023-03-21",
+			startTime: "17:00",
+			endDate: "2023-03-21",
+			endTime: "22:00",
+			timeZone: "Europe/Madrid",
+			location: "https://midufest.com",
+			buttonStyle: "round",
+			language: "es",
+			options: ["Google", "Apple", "iCal"],
+		};
 
-		atcb_action(config, e.target)
-	}
+		atcb_action(config, e.target);
+	};
 
 	const handleCopyLink = () => {
 		if (user?.username) {
-			navigator.clipboard.writeText(`https://midufest.com/ticket/${user.username}`)
-			import('https://cdn.skypack.dev/wc-toast')
-				.then(({ toast }) => toast('Enlace copiado al portapapeles', {
+			navigator.clipboard.writeText(
+				`https://midufest.com/ticket/${user.username}`
+			);
+			import("https://cdn.skypack.dev/wc-toast").then(({ toast }) =>
+				toast("Enlace copiado al portapapeles", {
 					duration: 2000,
 					icon: {
-						type: 'success'
-					}
-				}))
+						type: "success",
+					},
+				})
+			);
 		}
-	}
+	};
 
 	const handleTwitterShare = () => {
-		window.open('https://twitter.com/intent/tweet?text=%C2%A1Ya%20tengo%20mi%20ENTRADA%20para%20la%20%23MiduFest!%0AConferencia%20de%20Desarrollo%20y%20Programaci%C3%B3n%20Web.%0A%0A%C2%A1Consigue%20el%20tuyo%20totalmente%20gratis!%20%E2%87%A9&url=https%3A%2F%2Fmidufest.com%2Fticket%2Fmidudev')
-	}
+		window.open(
+			"https://twitter.com/intent/tweet?text=%C2%A1Ya%20tengo%20mi%20ENTRADA%20para%20la%20%23MiduFest!%0AConferencia%20de%20Desarrollo%20y%20Programaci%C3%B3n%20Web.%0A%0A%C2%A1Consigue%20el%20tuyo%20totalmente%20gratis!%20%E2%87%A9&url=https%3A%2F%2Fmidufest.com%2Fticket%2Fmidudev"
+		);
+	};
 
-	if (!user) return null
+	if (!user) return null;
 
 	return (
-		<footer class='flex justify-center items-center mt-10 gap-x-2'>
+		<footer class="flex justify-center items-center mt-10 gap-x-2">
 			<Button onClick={handleAddCalendar}>Añadir al calendario</Button>
 			<Button onClick={handleCopyLink}>Copiar enlace</Button>
-			<Button onClick={handleTwitterShare}>Compartir en Twitter</Button>
+			<TwitterButton onClick={handleTwitterShare}>
+				<Icons.twitter class="w-4 h-4 mr-2 -ml-1" /> Compartir en Twitter
+			</TwitterButton>
 		</footer>
-	)
+	);
 }
