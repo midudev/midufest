af1688e7c4fcbe2214f04450cf712edbef0572b3
Miguel Ángel Durán <miduga@gmail.com>
2023-02-02 16:46:43 -0300

    Add component to decide which ticket to show

---
+++ src/components/TicketBottom.tsx
@@ -0,0 +1,13 @@
+import { TicketAnonymous } from './TicketAnonymous'
+import { TicketUser } from './TicketUser'
+import { useUser } from '@hooks/useUser'
+
+export function TicketBottom () {
+	const { user } = useUser()
+
+	return (
+		user
+			? <TicketUser />
+			: <TicketAnonymous />
+	)
+}
