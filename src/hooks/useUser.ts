5bfc9d29e9507a0c1be3a1bf55b0f91fd2a480f1
Miguel Ángel Durán <miduga@gmail.com>
2023-02-17 12:58:00 +0100

    Create ticket when user registers

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -5,29 +5,65 @@ type User = {
 	avatar: string
 	name: string
 	username: string
-	id: string
+	id: string,
+	uuid: string
+}
+
+async function getTicketForUser (userId: string) {
+	const { data } = await supabase.from('tickets').select().eq('user_id', userId).single()
+	return data
+}
+
+export async function createTicketForUser (user: User) {
+	// check if user has ticket
+	const ticket = await getTicketForUser(user.uuid)
+	if (ticket) return ticket
+
+	const { data, error } = await supabase.from('tickets').insert(
+		{
+			user_id: user.uuid,
+			user_name: user.username,
+			user_fullname: user.name
+		}).select()
+
+	console.log(error)
+	return data
 }
 
 export const useUser = () => {
 	const [user, setUser] = useState<User | null>(null)
+	const [ticket, setTicket] = useState(null)
+
+	useEffect(() => {
+		async function getTicket () {
+			if (user) {
+				const ticket = await createTicketForUser(user)
+				setTicket(ticket)
+			}
+		}
+
+		getTicket()
+	}, [user])
 
 	useEffect(() => {
 		async function getUser () {
 			const { data: { user } } = await supabase.auth.getUser()
 			if (!user) return setUser(null)
 
-			const { identities } = user
+			const { identities, id: uuid } = user
 			if (!identities || !identities.length) return setUser(null)
 
 			const [github] = identities
 			const { id, identity_data: data } = github
 			const { avatar_url: avatar, name, user_name: username } = data
-			const newUser = { avatar, name, username, id }
+			const newUser = { avatar, name, username, id, uuid }
 			setUser(newUser)
 		}
 
-		const { data: { subscription } } = supabase.auth.onAuthStateChange((event) => {
-			if (event === 'SIGNED_OUT') setUser(null)
+		const { data: { subscription } } = supabase.auth.onAuthStateChange(async (event) => {
+			if (event === 'SIGNED_OUT') {
+				setUser(null)
+			}
 		})
 
 		getUser()
@@ -35,5 +71,5 @@ export const useUser = () => {
 		return () => subscription?.unsubscribe()
 	}, [])
 
-	return { user }
+	return { user, ticket }
 }
