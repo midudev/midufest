9ad08aff8b0e52d13debad5e4780a02dd25dc39f
Miguel Ángel Durán <miduga@gmail.com>
2023-02-17 12:54:30 +0100

    Add config to astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,17 @@
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
-import preact from "@astrojs/preact";
+import preact from '@astrojs/preact';
+
+// https://astro.build/config
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: vercel(),
   integrations: [tailwind(), preact()]
 });
