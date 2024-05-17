2731c0a8230d3f7e567e06c4eaffcbac103a9617
Miguel Ángel Durán <miduga@gmail.com>
2024-05-17 12:05:09 +0200

    Use new versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,9 @@
 import { defineConfig } from 'astro/config'
+
 import vercel from '@astrojs/vercel/serverless'
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
 import preact from '@astrojs/preact'
 
-// https://astro.build/config
-
-// https://astro.build/config
 export default defineConfig({
 	output: 'server',
 	adapter: vercel(),
